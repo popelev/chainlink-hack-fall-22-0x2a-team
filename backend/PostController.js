3f22fb3e5ee492dd2c293c4b026691decbaafd01
popelev.fedor <popelev.fedor@gmail.com>
2022-10-19 18:28:27 +0600

    v2

--- backend/PostController.js
+++ backend/PostController.js
@@ -1,12 +1,15 @@
 import ethers from "ethers";
-import erc20 from "./contracts/ERC20.json" assert { type: "json" };
+import Package from "./contracts/PackageToken.json" assert { type: "json" };
+import dotenv from "dotenv";
+dotenv.config();
 
-const ownerPrivateKey =
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const producerPrivateKey =
-  "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
-
-const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const INFURA_RPC_URL = process.env.INFURA_RPC_URL;
+const OWNER_PK = process.env.OWNER_PK;
+const MANAGER_PK = process.env.MANAGER_PK;
+const PRODUCER_PK = process.env.PRODUCER_PK;
+const SUPLIER_PK = process.env.SUPLIER_PK;
+const USER_PK = process.env.USER_PK;
+const PACKAGE_TRACKER_ADDRESS = process.env.PACKAGE_TRACKER_ADDRESS;
 
 class Answer {
   constructor(answer) {
@@ -20,40 +23,73 @@ class Postcontroller {
       // PREPARING
       const provider = new ethers.providers.JsonRpcProvider();
       ethers.Wallet.provider = provider;
-      let owner = new ethers.Wallet(ownerPrivateKey, provider);
-      let producer = new ethers.Wallet(producerPrivateKey, provider);
 
-      const ERC20 = new ethers.Contract(contractAddress, erc20.abi, provider);
+      let ownerWallet = new ethers.Wallet(OWNER_PK, provider);
+      let managerWallet = new ethers.Wallet(MANAGER_PK, provider);
+      let producerWallet = new ethers.Wallet(PRODUCER_PK, provider);
+      let suplierWallet = new ethers.Wallet(SUPLIER_PK, provider);
+      let userWallet = new ethers.Wallet(USER_PK, provider);
 
-      const ERC20ByOwner = ERC20.connect(owner);
-      const ERC20ByProducer = ERC20.connect(producer);
+      const TokenContract = new ethers.Contract(
+        PACKAGE_TRACKER_ADDRESS,
+        Package.abi,
+        provider
+      );
 
-      // LOGIC
-      let answerBody = "not found";
       let request = req.body;
 
-      if (request.account == "producer") {
-      } else if (request.account == "suplier") {
-        answerBody = "suplier";
-      } else if (request.account == "customer") {
-        answerBody = "customer";
-      }
-      answerBody = {
+      let answerBody = {
         answerId: request.id,
         qr: request.qr,
-        success: true,
-        imageLink:
-          "https://www.mordovmedia.ru/media/news/36/45636/fee13cffeffe19bc38cd022936c2269e.jpg",
-        blockchainLink:
-          "https://etherscan.io/tx/0xe6bbaac50d546499c43e0fd613277949ac66c1f02d3c58bd26a61dd8598113cd",
-        name: "Box with something awesome",
-        produced: true,
-        producedDate: "01.01.23 09:00",
-        inShop: true,
-        inShopDate: "02.01.23 09:00",
-        Sold: true,
-        soldDate: "03.01.23 09:00",
+        success: false,
+        imageLink: "",
+        blockchainLink: "",
+        name: "Not Found",
+        produced: false,
+        producedDate: "",
+        inShop: false,
+        inShopDate: "",
+        Sold: false,
+        soldDate: "",
       };
+
+      // LOGIC
+      //const ContractByOwner = TokenContract.connect(ownerWallet);
+      //const ContractByManager = TokenContract.connect(managerWallet);
+
+      const ContractByUser = TokenContract.connect(userWallet);
+      let tokenId = await ContractByUser.getTokenNumberByUniqueId(request.qr);
+      let scanAnswer;
+
+      if (request.type == "scan") {
+        scanAnswer = await ContractByUser.getTokenDetails(tokenId);
+      } else if (request.account == "producer") {
+        const ContractByProducer = TokenContract.connect(producerWallet);
+        if (request.type == "produce") {
+          const tx = await ContractByProducer.setProductionTimestamp(tokenId);
+          //await tx.wait();
+          scanAnswer = await ContractByProducer.getTokenDetails(tokenId);
+        }
+      } else if (request.account == "suplier") {
+        const ContractBySuplier = TokenContract.connect(suplierWallet);
+        if (request.type == "inshop") {
+          const tx = await ContractBySuplier.setInStockTimestamp(tokenId);
+          //await tx.wait();
+        } else if (request.type == "sale") {
+          const tx = await ContractBySuplier.setSoldTimestamp(tokenId);
+          //await tx.wait();
+        }
+        scanAnswer = await ContractBySuplier.getTokenDetails(tokenId);
+      }
+
+      let success = scanAnswer.state > 0;
+      if (success) {
+        answerBody.success = success;
+        answerBody.produced = scanAnswer.state >= 2;
+        answerBody.inShop = scanAnswer.state >= 3;
+        answerBody.Sold = scanAnswer.state >= 4;
+      }
+
       // ANSWER
       let answer = new Answer(answerBody);
       res.status(200).json(answer);
