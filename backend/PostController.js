50ecd874aad5551219ee8b99a35e9b116ea5c8b8
popelev.fedor <popelev.fedor@gmail.com>
2022-10-24 17:13:53 +0600

    v5

--- backend/PostController.js
+++ backend/PostController.js
@@ -15,6 +15,12 @@ const USER_PK = process.env.USER_PK;
 const PACKAGE_TRACKER_ADDRESS = process.env.PACKAGE_TRACKER_ADDRESS;
 const VRF_ADDRESS = process.env.VRF_ADDRESS;
 
+const NOT_MINTED = 0;
+const MINTED = 1;
+const PRODUCED = 2;
+const IN_STOCK = 3;
+const SOLD = 4;
+
 class Answer {
   constructor(answer) {
     this.answer = answer;
@@ -28,16 +34,20 @@ class Postcontroller {
     let answerBody = {
       answerId: request.id,
       qr: request.qr,
+      account: "user",
       success: false,
       imageLink: "",
       blockchainLink: "",
       name: "",
+      description: "",
       produced: false,
       producedDate: "",
       inShop: false,
       inShopDate: "",
       sold: false,
       soldDate: "",
+      buttonTitle: "",
+      buttonEnable: false,
     };
     let scanAnswer;
 
@@ -71,9 +81,7 @@ class Postcontroller {
       const ContractByUser = TokenContract.connect(userWallet);
       let tokenId = await ContractByUser.getTokenNumberByUniqueId(request.qr);
 
-      if (request.type == "scan") {
-        scanAnswer = await ContractByUser.getTokenDetails(tokenId);
-      } else if (request.account == "producer") {
+      if (request.account == "producer") {
         const ContractByProducer = TokenContract.connect(producerWallet);
         const vrgCoordinatorV2Mock = VRFConsumerBase.connect(ownerWallet);
         if (request.type == "mint") {
@@ -87,41 +95,101 @@ class Postcontroller {
           const count = await ContractByProducer.getTokenCounter();
           scanAnswer = await ContractByProducer.getTokenDetails(count);
         } else if (request.type == "produce") {
-          const tx = await ContractByProducer.setProductionTimestamp(tokenId);
-          //await tx.wait();
+          await ContractByProducer.setProductionTimestamp(tokenId);
+          scanAnswer = await ContractByProducer.getTokenDetails(tokenId);
+        } else {
           scanAnswer = await ContractByProducer.getTokenDetails(tokenId);
         }
       } else if (request.account == "suplier") {
         const ContractBySuplier = TokenContract.connect(suplierWallet);
         if (request.type == "inshop") {
-          const tx = await ContractBySuplier.setInStockTimestamp(tokenId);
-          //await tx.wait();
+          await ContractBySuplier.setInStockTimestamp(tokenId);
+          scanAnswer = await ContractBySuplier.getTokenDetails(tokenId);
         } else if (request.type == "sale") {
-          const tx = await ContractBySuplier.setSoldTimestamp(tokenId);
-          //await tx.wait();
+          await ContractBySuplier.setSoldTimestamp(tokenId);
+          scanAnswer = await ContractBySuplier.getTokenDetails(tokenId);
+        } else {
+          scanAnswer = await ContractBySuplier.getTokenDetails(tokenId);
         }
-        scanAnswer = await ContractBySuplier.getTokenDetails(tokenId);
+      } else {
+        scanAnswer = await ContractByUser.getTokenDetails(tokenId);
       }
 
-      let success = scanAnswer.state > 0;
+      // Date and Time
+      let success = scanAnswer.state > NOT_MINTED;
       if (success) {
         answerBody.qr = scanAnswer.uniqueId.toString();
+        answerBody.name = await ContractByUser.getTitle(scanAnswer.titleId);
+        answerBody.description = await ContractByUser.getDescription(
+          scanAnswer.descriptionId
+        );
+        answerBody.imageLink = await ContractByUser.getImageURI(
+          scanAnswer.imageUriId
+        );
+
         answerBody.success = success;
-        answerBody.produced = scanAnswer.state >= 2;
+        answerBody.produced = scanAnswer.state >= PRODUCED;
         if (answerBody.produced) {
-          answerBody.producedDate = new Date(scanAnswer.producedTime * 1000);
+          answerBody.producedDate = new Date(scanAnswer.producedTime * 1000)
+            .toISOString()
+            .split(".")[0]
+            .replace("T", " ");
+          answerBody.buttonTitle = "Produced";
+          answerBody.buttonEnable = false;
         }
-        answerBody.inShop = scanAnswer.state >= 3;
+        answerBody.inShop = scanAnswer.state >= IN_STOCK;
         if (answerBody.inShop) {
-          answerBody.inShopDate = new Date(scanAnswer.inStockTime * 1000);
+          answerBody.inShopDate = new Date(scanAnswer.inStockTime * 1000)
+            .toISOString()
+            .split(".")[0]
+            .replace("T", " ");
+          answerBody.buttonTitle = "In Stock";
+          answerBody.buttonEnable = false;
         }
 
-        answerBody.sold = scanAnswer.state >= 4;
+        answerBody.sold = scanAnswer.state >= SOLD;
         if (answerBody.sold) {
-          answerBody.soldDate = new Date(scanAnswer.soldTime * 1000);
+          answerBody.soldDate = new Date(scanAnswer.soldTime * 1000)
+            .toISOString()
+            .split(".")[0]
+            .replace("T", " ");
+          answerBody.buttonTitle = "Sold";
+          answerBody.buttonEnable = false;
+        }
+
+        // Button
+        answerBody.account = request.account;
+
+        if (answerBody.account == "producer") {
+          if (scanAnswer.state < MINTED) {
+            answerBody.buttonTitle = "Not minted";
+            answerBody.buttonEnable = false;
+          } else if (scanAnswer.state == MINTED) {
+            answerBody.buttonTitle = "Produce";
+            answerBody.buttonEnable = true;
+          } else {
+            answerBody.buttonTitle = "Produced";
+            answerBody.buttonEnable = false;
+          }
+        } else if (answerBody.account == "suplier") {
+          if (scanAnswer.state < PRODUCED) {
+            answerBody.buttonTitle = "Not produced";
+            answerBody.buttonEnable = false;
+          } else if (scanAnswer.state == PRODUCED) {
+            answerBody.buttonTitle = "In Stock";
+            answerBody.buttonEnable = true;
+          } else if (scanAnswer.state == IN_STOCK) {
+            answerBody.buttonTitle = "Sale";
+            answerBody.buttonEnable = true;
+          } else {
+            answerBody.buttonTitle = "Sold";
+            answerBody.buttonEnable = false;
+          }
         }
       } else {
         answerBody.name = "Not Found";
+        answerBody.buttonTitle = "Not Found";
+        answerBody.buttonEnable = false;
       }
 
       // ANSWER
