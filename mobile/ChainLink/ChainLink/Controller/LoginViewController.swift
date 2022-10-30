53d2c3d033e07ed4ed702b5106e298635e2d5a1f
Artem <a.bugrov@finch-melrose.com>
2022-10-30 22:21:20 +0300

    add project

---
+++ mobile/ChainLink/ChainLink/Controller/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  ChainLink
+//
+//  Created by Artem on 30.10.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
